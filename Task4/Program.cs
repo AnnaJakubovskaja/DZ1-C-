fd69291255bcb8eba4b75919f05b6dce1585e809
Anna Jakubovskaja <anna28.78@bk.ru>
2022-11-18 00:35:17 +0300

    Task4 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,3 +3,13 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
+Console.Write("Введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int negativeNumber=2;
+
+while (negativeNumber<=number)
+    {
+        Console.Write(negativeNumber+" ");
+        negativeNumber=negativeNumber+2;         
+    }
