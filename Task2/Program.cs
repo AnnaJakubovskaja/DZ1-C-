95221c74fd401d4512335245e57788af23c7643b
Anna Jakubovskaja <anna28.78@bk.ru>
2022-11-17 16:06:17 +0300

    Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,10 +7,10 @@
 Console.WriteLine("Введите первое число: ");
 int a=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
 int b=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите третье число: ");
 int c=Convert.ToInt32(Console.ReadLine());
 
 int max=a;
